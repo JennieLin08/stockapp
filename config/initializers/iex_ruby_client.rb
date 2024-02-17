44a239dea0d13a8fb8325015c4ed409680fd608e
JennieLin08 <jennelyn.hernandez2578@gmail.com>
2024-02-18 03:06:37 +0800

    update bg

--- config/initializers/iex_ruby_client.rb
+++ config/initializers/iex_ruby_client.rb
@@ -3,7 +3,7 @@
 IEX::Api.configure do |config|
   # config.publishable_token = 'pk_859b1e7fef6a47b49a68e6201e903e51'
   # config.secret_token = 'sk_ff034c1aa0364fe7a5c0dfce3c8abb87'
-  config.publishable_token = 'pk_9041ba4b55a84ac1920cc1d1721d4d18'
-  config.secret_token = 'sk_48313dad6eb24df682c06708a987ba0e'
+  config.publishable_token = 'pk_97f47442e14d419babeb4987a53dc64c'
+  config.secret_token = 'sk_a3b887647f5b4fa784e2c82eca01e8c4'
   config.endpoint = 'https://cloud.iexapis.com/v1' # or the appropriate API endpoint
 end
